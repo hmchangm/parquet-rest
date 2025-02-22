f1d449f7b3471787178d181454d330aa0b90aef4
hmchangm <dickgg@gmail.com>
2025-02-22 21:36:02 +0800

    change sample data to user

--- src/main/kotlin/tw/idv/brandy/ParquetResource.kt
+++ src/main/kotlin/tw/idv/brandy/ParquetResource.kt
@@ -5,7 +5,6 @@ import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
-import jakarta.ws.rs.core.Response
 import org.jboss.resteasy.reactive.RestPath
 
 @Path("/query")
@@ -19,35 +18,5 @@ class ParquetResource(
     fun queryWithParams(
         @RestPath queryName: String,
         body: Map<String, Any>,
-    ): Response =
-        runCatching {
-            parquetService.executeNamedQuery(queryName, body)
-        }.fold(
-            onSuccess = { Response.ok(it).build() },
-            onFailure = { e ->
-                when (e) {
-                    is IllegalArgumentException ->
-                        Response.status(Response.Status.BAD_REQUEST).entity(e.message).build()
-                    else ->
-                        Response
-                            .status(Response.Status.INTERNAL_SERVER_ERROR)
-                            .entity("Error executing query: ${e.message}")
-                            .build()
-                }
-            },
-        )
-
-    @POST
-    @Path("/refresh")
-    @Produces(MediaType.TEXT_PLAIN)
-    fun refreshParquetFile(): Response =
-        try {
-           // parquetService.fetchParquetFile()
-            Response.ok("Parquet file refreshed successfully").build()
-        } catch (e: Exception) {
-            Response
-                .status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity("Error refreshing parquet file: ${e.message}")
-                .build()
-        }
+    ) = parquetService.executeNamedQuery(queryName, body)
 }
--- src/test/kotlin/tw/idv/brandy/GreetingResourceTest.kt
+++ src/test/kotlin/tw/idv/brandy/GreetingResourceTest.kt
@@ -1,19 +1,22 @@
 package tw.idv.brandy
 
 import io.quarkus.test.junit.QuarkusTest
-import io.restassured.RestAssured.given
+import io.restassured.module.kotlin.extensions.Given
+import io.restassured.module.kotlin.extensions.Then
+import io.restassured.module.kotlin.extensions.When
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
 class GreetingResourceTest {
     @Test
     fun testHelloEndpoint() {
-        given()
-            .body("{}")
-            .header("Content-Type", "application/json")
-            .`when`()
-            .post("/query/get-all")
-            .then()
-            .statusCode(200)
+        Given {
+            body("{}")
+            header("Content-Type", "application/json")
+        } When {
+            post("/query/get-all")
+        } Then {
+            statusCode(200)
+        }
     }
 }
